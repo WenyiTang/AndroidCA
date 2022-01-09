4336b557bb16e5a93a970dd0f100977a3361406b
CY <69207419+kcy-23@users.noreply.github.com>
2022-01-09 23:43:21 +0800

    Updated MainActivity screen to latest

---
+++ app/src/main/java/iss/workshop/ca/Guide.java
@@ -0,0 +1,14 @@
+package iss.workshop.ca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Guide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
--- app/src/main/java/iss/workshop/ca/MainActivity.java
+++ app/src/main/java/iss/workshop/ca/MainActivity.java
@@ -9,29 +9,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button startBtn;
-    private Button seletcBtn;
+    private Button guideBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startBtn = findViewById(R.id.playGame);
+        startBtn = findViewById(R.id.selectImage);
         startBtn.setOnClickListener(this);
 
-        seletcBtn = findViewById(R.id.selectImage);
-        seletcBtn.setOnClickListener(this);
+        guideBtn = findViewById(R.id.guideBtn);
+        guideBtn.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         if (view == startBtn){
-            Intent intent = new Intent(this, GamePlay.class);
+            Intent intent = new Intent(this, LoadingImageActivity.class);
             startActivity(intent);
             finish();
-        }else if(view == seletcBtn){
-            Intent intent = new Intent(this, LoadingImageActivity.class);
+        }else if(view == guideBtn){
+            Intent intent = new Intent(this, Guide.class);
             startActivity(intent);
         }
     }
