c3cbf49450b0af995f520b64b08ef3d261a8d8ea
glenna <glennatjt@gmail.com>
2022-01-09 11:29:53 +0800

    add background music

--- app/src/main/java/iss/workshop/ca/MainActivity.java
+++ app/src/main/java/iss/workshop/ca/MainActivity.java
@@ -3,12 +3,15 @@ package iss.workshop.ca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button startBtn, guideBtn;
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         guideBtn = findViewById(R.id.guideBtn);
         guideBtn.setOnClickListener(this);
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.background);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
     }
 
     @Override
